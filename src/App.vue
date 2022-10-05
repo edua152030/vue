dfe0f11164190f21bfef89ab93699643151e892d
edua152030 <eduardo.vaz.faria@gmail.com>
2022-10-05 10:49:57 -0300

    primeira inserção

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <p>Aplicação aula</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component
+
+export default class App extends Vue {}
+</script>
+
